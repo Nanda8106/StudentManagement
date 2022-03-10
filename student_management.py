ca4f685293b113bf262400678f0773936d131c9b
Nanda <mandlenandakishore89@gmail.com>
2022-03-10 21:05:03 +0100

    Add test case for adding students by Nandakishore Reddy Mandle

--- student_management.py
+++ student_management.py
@@ -1,34 +1,11 @@
 class StudentManagement:
 
     def __init__(self):
+        # we will store students details in a list
         self.students = []
 
+    # writing functionality for adding student
     def add_student(self, name, age, course):
-        self.students.append({"name": name, "age":age, "course":course})
-        return str(f"{name} details added")
-
-    def update_student(self, name, age, course):
-        for index in range(len(self.students)):
-            if self.students[index]["name"] == name:
-                self.students[index] = {"name": name, "age": age, "course": course}
-                return str(f"{name} details updated")
-        else:
-            return "No data found"
-
-    def delete_student(self, name):
-        for index in range(len(self.students)):
-            if self.students[index]["name"] == name:
-                self.students.pop(index)
-                return str(f"{name} details deleted")
-        else:
-            return "No data found"
-
-    def view_details(self, name):
-        for index in range(len(self.students)):
-            if self.students[index]["name"] == name:
-                age = self.students[index]["age"]
-                course = self.students[index]["course"]
-                return str(f"Student details of {name} are age={age}, course = {course}")
-        else:
-            return "No data found"
-
+        self.students.append({"name":name, "age":age, "course": course})
+        # return str(f"{name} details added")
+        return "nothing"
--- test/test_student_management.py
+++ test/test_student_management.py
@@ -7,6 +7,7 @@ class TestStudentManagement(unittest.TestCase):
     def setUp(self):
         self.manager = StudentManagement()
 
+    # this is not a test case, it is a function to set data
     def set_data(self):
         name = "nanda"
         age = 20
@@ -14,37 +15,13 @@ class TestStudentManagement(unittest.TestCase):
         self.manager.add_student(name, age, course)
         self.assertEqual(len(self.manager.students), 1)
 
+    # writing test case for adding_Students
     def test_add_student(self):
         self.set_data()
         name = "kishore"
         age = 20
-        course = "mechanical"
+        course = "Mechanical"
         output = self.manager.add_student(name, age, course)
-        expected_output = str(f"{name} details addedd")
+        expected_output = str(f"{name} details added")
         self.assertEqual(expected_output, output)
 
-    def test_view_details(self):
-        self.set_data()
-        data = self.manager.students[0]
-        name = data["name"]
-        output = self.manager.view_details(name)
-        expected_output = str(f"Student details of {name} are age=20, course = computers")
-        self.assertEqual(expected_output, output)
-
-    def test_update_student(self):
-        self.set_data()
-        data = self.manager.students[0]
-        name = data["name"]
-        age = data["age"]
-        course = data["course"]
-        output = self.manager.update_student(name, age, course)
-        expected_output = str(f"{name} details updated")
-        self.assertEqual(expected_output, output)
-
-    def test_delete_student(self):
-        self.set_data()
-        data = self.manager.students[0]
-        name = data["name"]
-        output = self.manager.delete_student(name)
-        expected_output = str(f"{name} details deleted")
-        self.assertEqual(expected_output, output)
